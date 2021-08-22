f5549615ae1da27d707367ecad4495a2e13d478e
ahmedKhaireddine <ahmed_khaireddine@yahoo.com>
2021-08-22 16:06:55 +0200

    Add apollo server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,29 @@
 import express from "express";
+import { ApolloServer, gql } from 'apollo-server-express';
 import config from "./config";
 
-const app = express();
+(async () => {
+  const app = express();
 
-app.listen(config.port, () => {
-  console.log(`🚀  Server ready at http://localhost:${config.port}`);
-});
+  const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+  `;
+
+  const resolvers = {
+    Query: {
+      hello: () => 'Hello world!',
+    },
+  };
+
+  const server = new ApolloServer({ typeDefs, resolvers });
+
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  app.listen(config.port, () => {
+    console.log(`🚀  Server ready at http://localhost:${config.port}${ server.graphqlPath }`);
+  });
+})();
