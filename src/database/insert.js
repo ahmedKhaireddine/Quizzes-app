e3ca64a8687720aeeb5131b71634b3da6d9b0074
ahmedKhaireddine <ahmed_khaireddine@yahoo.com>
2021-08-25 16:37:12 +0200

    Insert questions data into database

--- src/database/insert.js
+++ src/database/insert.js
@@ -1,5 +1,7 @@
 import databaseConnect from "./connect";
+import questions from "../data/questions.json";
 import topics from "../data/topics.json";
+import Question from "./models/Question";
 import Topic from "./models/Topic";
 
 (async () => {
@@ -11,6 +13,17 @@ import Topic from "./models/Topic";
     await Topic.deleteMany();
 
     const topicsInsered = await Topic.insertMany(topics);
+
+    // Insertion of questions
+    await Question.deleteMany();
+
+    const questionsToInsert = questions.map( question => {
+      question.topic = topicsInsered.find( topic => topic.title === question.topic )._id;
+
+      return question;
+    });
+
+    const questionsInsered = await Question.insertMany(questionsToInsert);
   } catch (error) {
     console.log(error);
   }
