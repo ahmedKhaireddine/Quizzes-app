b437043ccc062e4ef20f42d8a224d75b47ae1313
ahmedKhaireddine <ahmed_khaireddine@yahoo.com>
2021-09-18 20:14:01 +0200

    Add system of steps

--- client/src/pages/QuizArea.jsx
+++ client/src/pages/QuizArea.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import AnimatedCircles from "../components/quiz/AnimatedCircles";
 import "./QuizArea.css";
 
+const stepper = (step) => {
+  switch (step) {
+    case 1:
+      return <h1 style={{ color: "white" }}>Step 1</h1>;
+    case 2:
+      return <h1 style={{ color: "white" }}>Step 2</h1>;
+    case 3:
+      return <h1 style={{ color: "white" }}>Step 3</h1>;
+    default:
+      return <h1 style={{ color: "white" }}>Unknown step</h1>;
+  }
+}
+
 const QuizArea = () => {
+  const [step, setStep] = useState(1)
+
   return (
     <div className="container">
+      {stepper(step)}
       <AnimatedCircles />
     </div>
   )
