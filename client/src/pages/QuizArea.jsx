9212086cbf788f8b36b6b0b02b8340c2b718896c
ahmedKhaireddine <ahmed_khaireddine@yahoo.com>
2021-10-11 16:20:37 +0200

    Add most of the components of the quiz steps

--- client/src/pages/QuizArea.jsx
+++ client/src/pages/QuizArea.jsx
@@ -2,21 +2,24 @@ import React, { useState } from "react";
 import AnimatedCircles from "../components/quiz/AnimatedCircles";
 import Room from "../components/quiz/Room";
 import Search from "../components/quiz/Search";
+import PlayerInfo from "../components/quiz/PlayerInfo";
 import "./QuizArea.css";
 
 
 const QuizArea = () => {
-  const [step, setStep] = useState(2);
+  const [step, setStep] = useState(1);
   const [score, setScore] = useState(0);
 
   const stepper = (step) => {
     switch (step) {
       case 1:
-        return <Search />
+        return <Search setStep={setStep}/>
       case 2:
-        return <Room setScore={setScore} setStep={setStep} step={step}/>;
+        return <PlayerInfo setStep={setStep}/>
       case 3:
-        return <h1 style={{ color: "white" }}>Step 3 and Score {score}</h1>;
+        return <Room setScore={setScore} setStep={setStep} step={step}/>;
+      case 4:
+        return <h1 style={{ color: "white" }}>Step 4 and Score {score}</h1>;
       default:
         return <h1 style={{ color: "white" }}>Unknown step</h1>;
     }
