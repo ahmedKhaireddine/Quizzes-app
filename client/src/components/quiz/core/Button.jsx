23400b6e44d065d86a42638f06a072108b5cf74b
ahmedKhaireddine <ahmed_khaireddine@yahoo.com>
2021-11-03 10:16:54 +0100

    Add type props to Button component

--- client/src/components/quiz/core/Button.jsx
+++ client/src/components/quiz/core/Button.jsx
@@ -2,24 +2,36 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 const Button = (props) => {
-  const { className, children, onFunction } = props;
+  const {
+    className,
+    children,
+    onFunction,
+    type
+  } = props;
 
-  return <button
-    className={className}
-    onClick={() => onFunction()}
-  >{children}</button>
+  return (
+    <button
+      className={className}
+      type={type}
+      onClick={() => onFunction()}
+    >
+      {children}
+    </button>
+  )
 }
 
 Button.defaultProps = {
   className: "class à definir",
   children: "Name a definir",
-  onFunction: () => {}
+  onFunction: () => {},
+  type: "button"
 }
 
 Button.protoTypes = {
   className: PropTypes.string,
   children: PropTypes.element.isRequired,
-  onFunction: PropTypes.func
+  onFunction: PropTypes.func,
+  type: PropTypes.string
 }
 
 export default Button;
